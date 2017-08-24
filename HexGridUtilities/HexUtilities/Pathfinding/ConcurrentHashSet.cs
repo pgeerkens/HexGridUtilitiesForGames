bfc2e3cc1b5083b962afc1366e8114d6705c4349
Pieter Geerkens <pgeerkens@hotmail.com>
2017-08-24 15:58:25 -0400

    Alternate Refectoring for PathFinding - needs to be merged with first.

--- HexGridUtilities/HexUtilities/Pathfinding/ConcurrentHashSet.cs
+++ HexGridUtilities/HexUtilities/Pathfinding/ConcurrentHashSet.cs
@@ -1,4 +1,4 @@
-﻿#region The MIT License - Copyright (C) 2012-2014 Pieter Geerkens
+﻿#region The MIT License - Copyright (C) 2012-2013 Pieter Geerkens
 /////////////////////////////////////////////////////////////////////////////////////////
 //                PG Software Solutions Inc. - Hex-Grid Utilities
 /////////////////////////////////////////////////////////////////////////////////////////
@@ -29,20 +29,16 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-
 using System.Diagnostics;
-using System.Diagnostics.CodeAnalysis;
 
-namespace PGNapoleonics.HexUtilities.Pathfinding {
+namespace PGNapoleonics.HexUtilities.PathFinding {
   /// <summary>Provides a thread-safe hash set data structure.</summary>
   /// <typeparam name="TKey">Specifies the type of elements in the hash set.</typeparam>
-  /// <typeparam name="TValue">Type of the element value in the hash set.</typeparam>
-  [SuppressMessage("Microsoft.Naming", "CA1710:IdentifiersShouldHaveCorrectSuffix")]
-  [DebuggerDisplay("Count={Count}")]
+  [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1710:IdentifiersShouldHaveCorrectSuffix"), DebuggerDisplay("Count={Count}")]
   public class ConcurrentHashSet<TKey, TValue> : ISet<TKey> where TKey : IEquatable<TKey>
   {
-    private readonly HashSet<TKey> _hashSet  = new  HashSet<TKey>();
-    private readonly object        _syncLock = new object();
+    private readonly object _syncLock = new object();
+    private readonly HashSet<TKey> _hashSet = new  HashSet<TKey>();
 
     /// <summary>Initializes a new instance of the <c>ConcurrentHashSet</c> class.</summary>
     public ConcurrentHashSet() {}
@@ -57,57 +53,56 @@ namespace PGNapoleonics.HexUtilities.Pathfinding {
       foreach (var item in collection) _hashSet.Add(item);
     }
 
-    /// <inheritdoc/>
-    public int                     Count      { get { lock (_syncLock) return _hashSet.Count; } }
+    ///<inheritdoc/>
+    public int Count { get { lock (_syncLock) return _hashSet.Count; } }
 
-    /// <inheritdoc/>
-    public IEqualityComparer<TKey> Comparer   { get { lock (_syncLock) return _hashSet.Comparer; } }
+    ///<inheritdoc/>
+    public IEqualityComparer<TKey> Comparer { get { lock (_syncLock) return _hashSet.Comparer; } }
 
-    /// <inheritdoc/>
-    public bool                    IsReadOnly { get { lock (_syncLock) return false; } }
+    ///<inheritdoc/>
+    public bool IsReadOnly { get { lock (_syncLock) return false; } }
 
-    /// <inheritdoc/>
+    ///<inheritdoc/>
     bool ISet<TKey>.Add(TKey item) { lock (_syncLock) return _hashSet.Add(item); }
-    /// <inheritdoc/>
+    ///<inheritdoc/>
     public void Add(TKey item) { lock (_syncLock) _hashSet.Add(item); }
 
-    /// <inheritdoc/>
+    ///<inheritdoc/>
     public void Clear() { lock(_syncLock) _hashSet.Clear(); }
 
-    /// <inheritdoc/>
+    ///<inheritdoc/>
     public bool Contains(TKey item) { lock (_syncLock) return _hashSet.Contains(item); }
 
-    /// <inheritdoc/>
+    ///<inheritdoc/>
     public void CopyTo(TKey[] array)
     {
       lock (_syncLock) _hashSet.CopyTo(array);
     }
-    /// <inheritdoc/>
+    ///<inheritdoc/>
     public void CopyTo(TKey[] array, int index, int count)
     {
       lock (_syncLock) _hashSet.CopyTo(array, index, count);
     }
-    /// <inheritdoc/>
+    ///<inheritdoc/>
     public void CopyTo(TKey[] array, int arrayIndex)
     {
       lock (_syncLock) _hashSet.CopyTo(array, arrayIndex);
     }
 
-    /// <inheritdoc/>
-    [SuppressMessage("Microsoft.Design", "CA1006:DoNotNestGenericTypesInMemberSignatures")]
+    ///<inheritdoc/>
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1006:DoNotNestGenericTypesInMemberSignatures")]
     public IEqualityComparer<HashSet<TKey>> CreateSetComparer() { 
       return HashSet<TKey>.CreateSetComparer(); 
     }
 
-    /// <inheritdoc/>
+    ///<inheritdoc/>
     public void ExceptWith(IEnumerable<TKey> other) {
       lock (_syncLock) _hashSet.ExceptWith(other);
     }
 
-    /// <inheritdoc/>
+    ///<inheritdoc/>
     IEnumerator IEnumerable.GetEnumerator() { return GetEnumerator(); }
-
-    /// <inheritdoc/>
+    ///<inheritdoc/>
     public IEnumerator<TKey> GetEnumerator()
     {
         var arr = new TKey[_hashSet.Count];
@@ -115,74 +110,75 @@ namespace PGNapoleonics.HexUtilities.Pathfinding {
         return ((IEnumerable<TKey>)arr).GetEnumerator();
     }
 
-    /// <inheritdoc/>
+    ///<inheritdoc/>
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA1801:ReviewUnusedParameters", MessageId = "context"), System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA1801:ReviewUnusedParameters", MessageId = "info"), System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA1801:ReviewUnusedParameters", MessageId = "context"), System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA1801:ReviewUnusedParameters", MessageId = "info")]
     public void GetObjectData(
       System.Runtime.Serialization.SerializationInfo info,
-      System.Runtime.Serialization.StreamingContext  context) {
-        lock (_syncLock) _hashSet.GetObjectData(info, context);
+      System.Runtime.Serialization.StreamingContext context) {
+      throw new NotSupportedException();
+//        lock (_syncLock) _hashSet.GetObjectData(info, context);
     }
 
-    /// <inheritdoc/>
+    ///<inheritdoc/>
     public void IntersectWith (IEnumerable<TKey> other) {
       lock (_syncLock) _hashSet.IntersectWith(other);
     }
 
-    /// <inheritdoc/>
+    ///<inheritdoc/>
     public bool IsProperSubsetOf (IEnumerable<TKey> other) {
       lock (_syncLock) return _hashSet.IsProperSubsetOf(other);
     }
 
-    /// <inheritdoc/>
+    ///<inheritdoc/>
     public bool IsProperSupersetOf (IEnumerable<TKey> other) {
       lock (_syncLock) return _hashSet.IsProperSupersetOf(other);
     }
 
-    /// <inheritdoc/>
+    ///<inheritdoc/>
     public bool IsSubsetOf (IEnumerable<TKey> other) {
       lock (_syncLock) return _hashSet.IsSubsetOf(other);
     }
 
-    /// <inheritdoc/>
+    ///<inheritdoc/>
     public bool IsSupersetOf (IEnumerable<TKey> other) {
       lock (_syncLock) return _hashSet.IsSupersetOf(other);
     }
 
-    /// <inheritdoc/>
     public virtual void OnDeserialization(Object sender) {
-      lock (_syncLock) _hashSet.OnDeserialization(sender);
+      throw new NotSupportedException();
     }
 
-    /// <inheritdoc/>
+    ///<inheritdoc/>
     public bool Overlaps (IEnumerable<TKey> other) {
       lock (_syncLock) return _hashSet.Overlaps(other);
     }
 
-    /// <inheritdoc/>
+    ///<inheritdoc/>
     public bool Remove (TKey item) {
       lock (_syncLock) return _hashSet.Remove(item);
     }
 
-    /// <inheritdoc/>
+    ///<inheritdoc/>
     public int RemoveWhere (Predicate<TKey> match) {
       lock (_syncLock) return _hashSet.RemoveWhere(match);
     }
 
-    /// <inheritdoc/>
+    ///<inheritdoc/>
     public bool SetEquals (IEnumerable<TKey> other) {
       lock (_syncLock) return _hashSet.SetEquals(other);
     }
 
-    /// <inheritdoc/>
+    ///<inheritdoc/>
     public void SymmetricExceptWith (IEnumerable<TKey> other) {
       lock (_syncLock) _hashSet.SymmetricExceptWith(other);
     }
 
-    /// <inheritdoc/>
+    ///<inheritdoc/>
     public void TrimExcess() {
       lock (_syncLock) _hashSet.TrimExcess();
     }
 
-    /// <inheritdoc/>
+    ///<inheritdoc/>
     public void UnionWith (IEnumerable<TKey> other) {
       lock (_syncLock) _hashSet.UnionWith(other);
     }
