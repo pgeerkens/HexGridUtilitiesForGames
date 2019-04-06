95c119e09237ab8647edff27051a54f6d040456f
Pieter Geerkens <pgeerkens@users.noreply.github.com>
2019-04-06 14:56:54 -0400

    Update ImmutableStack.cs

--- HexUtilities/Common/ImmutableStack.cs
+++ HexUtilities/Common/ImmutableStack.cs
@@ -35,26 +35,27 @@ namespace PGNapoleonics.HexUtilities.Common {
     /// <a href="http://blogs.msdn.com/b/ericlippert/archive/2007/10/04/path-finding-using-a-in-c-3-0-part-two.aspx">Path Finding Using A* Part THree</a>
     /// <typeparam name="T"></typeparam>
     public class ImmutableStack<T> : IEnumerable<T> {
-
-        /// <summary>Gets the top item on the stack.</summary>
-        public T                 TopItem      { get; private set; }
-        /// <summary>Gets the remainder of the stack.</summary>
-        public ImmutableStack<T> Remainder    { get; private set; }
-        /// <summary>Returns a new ImmutableStack by adding <paramref name="item"/> to this stack.</summary>
-        public ImmutableStack<T> Push(T item)
-        => new ImmutableStack<T>(item, this);
-
         /// <summary>Construct a new empty instance.</summary>
         public ImmutableStack(T start) : this(start, null) {}
+
         /// <summary>Construct a new instance by Push-ing <paramref name="item"/> onto <paramref name="remainder"/>.</summary>
         private ImmutableStack(T item, ImmutableStack<T> remainder) {
             TopItem   = item;
             Remainder = remainder;
         }
 
+        /// <summary>Gets the top item on the stack.</summary>
+        public T                 TopItem      { get; }
+
+        /// <summary>Gets the remainder of the stack.</summary>
+        public ImmutableStack<T> Remainder    { get; }
+
+        /// <summary>Returns a new ImmutableStack by adding <paramref name="item"/> to this stack.</summary>
+        public ImmutableStack<T> Push(T item) => new ImmutableStack<T>(item, this);
+
         /// <summary>Returns the stackitems in order from top to bottom.</summary>
         public IEnumerator<T> GetEnumerator() {
-            for (var p = this; p != null; p = p.Remainder)  yield return p.TopItem;
+            for (var p = this; p != null; p = p.Remainder) { yield return p.TopItem; }
         }
 
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
